33806e99165321546616e3e0437b82180fe19700
kamarel <kamarelngunda@gmail.com>
2025-02-27 22:36:19 -0500

    thanks

--- src/main/java/com/thanksgiving/thanksgivingservice/Service/ThanksGivingServiceImp.java
+++ src/main/java/com/thanksgiving/thanksgivingservice/Service/ThanksGivingServiceImp.java
@@ -93,7 +93,7 @@ public class ThanksGivingServiceImp implements ThanksGivingService{
     @Override
     public ApiResponseDto getAllMembers() {
         Mono<List<MembersDto>> listMono = webClient.get()
-                .uri("https://observant-courtesy-production.up.railway.app/api/v1/members")
+                .uri("https://strong-alignment-production.up.railway.app/api/v1/members")
                 .retrieve()
                 .bodyToFlux(MembersDto.class)
                 .collectList();
